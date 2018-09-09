9b47d0c19f8df3b7db17c7c2588c7b756b465c82
Olivia Meiring <olivia@yummy-wakame.com>
2018-09-08 20:21:36 -0700

    Animated splash screen

--- app/src/main/java/com/yummywakame/bookinventory2/SplashActivity.java
+++ app/src/main/java/com/yummywakame/bookinventory2/SplashActivity.java
@@ -2,7 +2,11 @@ package com.yummywakame.bookinventory2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * BookInventory2
@@ -11,11 +15,32 @@ import android.support.v7.app.AppCompatActivity;
  * olivia@yummy-wakame.com
  */
 public class SplashActivity extends AppCompatActivity {
+    private static final int SECONDS_DELAY = 5000;
+    private ImageView gifImaveView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
+        // Show animated gif logo using Glide
+        gifImaveView = findViewById(R.id.logo);
+        Glide.with(this).asGif()
+                .load(R.drawable.animated_logo)
+                .into(gifImaveView);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // After 5 seconds go to next activity
+                launchNextActivity();
+            }
+        }, SECONDS_DELAY);   // 5 seconds
+
+    }
+
+    // Launches Catalog Activity
+    private void launchNextActivity() {
         Intent intent = new Intent(this, CatalogActivity.class);
         startActivity(intent);
         finish();
